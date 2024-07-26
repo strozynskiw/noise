e7c7082e64e87abd9857bfd243c9f30eae33aa22
Wojtek <strozynskiw@gmail.com>
2024-07-26 22:01:42 +0200

    Task2 readme and better handling

--- task2/README.md
+++ task2/README.md
@@ -1,4 +1,10 @@
-Based on wasm-bindgen examples
+How to start the solution?
+=========================
+All commands should be executed from `task2` directory.
+
+```
+cd task2
+```
 
 Install wasm-pack
 
@@ -7,6 +13,7 @@ $ cargo install wasm-pack
 ```
 
 Build locally
+
 ```
 $ wasm-pack build --target web
 ```
@@ -14,7 +21,31 @@ $ wasm-pack build --target web
 You can build the example locally with:
 
 ```
-$ npm run serve
+npx http-server .
 ```
 
 and then visiting http://localhost:8080 in a browser should run the example!
+
+
+Echo server
+===========
+I found a good local echo server that you will need to run this example.
+You can check out this example:
+
+https://github.com/snapview/tokio-tungstenite/blob/master/examples/echo-server.rs
+
+To run this example just clone the repository and got to its location and run:
+```
+cargo run --example echo-server 127.0.0.1:12345
+```
+
+Now use `ws://localhost:12345/` as connection url and verify if the provided solution works correctly.
+
+
+Comments
+========
+I'm new to web assembly in rust and js environment in general. Fortunately it's so common and well known environment that it wasn't that hard to find good examples.
+The test is missing as I did not wanted to go too deep with that due to lack of time.
+
+
+Based on wasm-bindgen examples
--- task2/src/lib.rs
+++ task2/src/lib.rs
@@ -1,4 +1,4 @@
-use futures_util::{future, pin_mut, SinkExt, StreamExt};
+use futures_util::{SinkExt, StreamExt};
 use wasm_bindgen::prelude::*;
 use wasm_bindgen_futures::future_to_promise;
 use web_sys::console;
@@ -6,10 +6,11 @@ use web_sys::console;
 #[wasm_bindgen]
 pub fn ws_ping(endpoint: String, message: String) -> js_sys::Promise {
     future_to_promise(async move {
-        console::log_1(&"Test".into());
-        let ws = tokio_tungstenite_wasm::connect("ws://127.0.0.1:12345")
+        console::log_1(&"Trying to make a connection".into());
+        let ws = tokio_tungstenite_wasm::connect(endpoint)
             .await
-            .unwrap();
+            .map_err(|e| JsValue::from_str(&e.to_string()))?;
+
         let (mut sender, mut receiver) = ws.split();
 
         console::log_1(&"Connected to echo server.".into());
@@ -17,14 +18,18 @@ pub fn ws_ping(endpoint: String, message: String) -> js_sys::Promise {
         sender
             .send(tokio_tungstenite_wasm::Message::text(message.as_str()))
             .await
-            .unwrap();
+            .map_err(|e| JsValue::from_str(&e.to_string()))?;
 
         console::log_1(&"Sent payload to echo server. Waiting for response...".into());
 
-        let msg = receiver.next().await.unwrap().unwrap();
-        assert_eq!(msg, tokio_tungstenite_wasm::Message::text(message.as_str()));
+        let msg = receiver
+            .next()
+            .await
+            .ok_or(JsValue::from_str("No data"))?
+            .map_err(|e| JsValue::from_str(&format!("Error while receiving data: {e}")))?;
 
         console::log_1(&"Received and validated response.".into());
+
         return Ok(JsValue::from_str(&msg.to_string()));
     })
 }
